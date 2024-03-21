89b70a623c861dd0e5100e8db5333477f7a1062f
bailey chard <87585491+baileychard01@users.noreply.github.com>
2024-03-21 17:18:53 +0000

    one of my trends alcohol consumption compared to grade
    simple bar chart-not will update to make better

---
+++ grade-alcoholConsumption.py
@@ -0,0 +1,26 @@
+# Import necessary libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the datasets into pandas DataFrames
+portuguese = pd.read_csv('por2.csv')
+maths = pd.read_csv('mat2.csv')
+
+# Group the 'maths' DataFrame by alcohol consumption and calculate the mean grades
+alcoholToGrades = maths.groupby("Walc")["G3"].mean()
+
+# Create a bar plot to visualize the relationship between alcohol consumption and grades
+ax = alcoholToGrades.plot(kind='bar', xlabel='alcohol consumption (1 low - 5 high)', ylabel='grades',title='grade changes based on alcohol consumption')
+
+# Add grid to the plot
+ax.grid(True)
+
+# Annotate each bar with its corresponding grade value
+for i, grade in enumerate(alcoholToGrades):
+    ax.text(i, grade + 0.1, f"{grade:.2f}", ha='center', va='bottom')
+
+# Rotate x-axis labels for better readability
+plt.xticks(rotation=0)
+
+# Display the plot
+plt.show()
