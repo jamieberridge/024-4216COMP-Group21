9541610b9989668e09dd9fcb532d31b10c9d4349
bailey chard <87585491+baileychard01@users.noreply.github.com>
2024-03-29 01:15:07 +0000

    Update main.py
    added the rest of my visualisations

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-userInput = input("choose (1)weekend alcohol consumption and grade(2)male and female grades")
+userInput = input("choose (1)weekend alcohol consumption and grade(2)age distribution of students (3)Relationship Status by Parental Cohabitation (4)travel vs study time")
 
 if userInput == "1":
     with open("grade-alcoholConsumption.py") as file:
         exec(file.read())
 elif userInput == "2":
-    with open("start.py") as file:
+    with open("histogram.py") as file:
+        exec(file.read())
+elif userInput == "3":
+    with open("romantic.py") as file:
+        exec(file.read())
+elif userInput == "4":
+    with open("studyTime.py") as file:
         exec(file.read())
 
