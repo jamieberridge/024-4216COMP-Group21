cb7a068f5de273d14bb7b6b6b64c5a75ab817a99
jamieberridge <156792628+jamieberridge@users.noreply.github.com>
2024-04-01 15:25:09 +0100

    Updated for improved ease of use. Added a loop so the user could view multiple trends without rerunning. Made the visualisations' names into a list for better manipulation.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# List of visualisations formatted in a list to allow for easy manipulation
+# List of visualisations formatted in a list to allow for easy manipulation 
 listOfVisualisations = [
     # Bailey's trends
     "(1) Weekend alcohol consumption and grade", 
@@ -9,7 +9,7 @@ listOfVisualisations = [
     "(0) Exit"
     ]
 
-# Loop to  display the visualisation options and allow the user to select another option or exit
+# Loop to display the visualisation options and allow the user to select another option or exit
 while True:
     print("Choose a visualisation from the following: ")
     for x in listOfVisualisations:  
@@ -30,4 +30,3 @@ while True:
     elif userInput == "4":
         with open("studyTime.py") as file:
             exec(file.read())
-
