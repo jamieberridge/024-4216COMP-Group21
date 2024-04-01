62cfab631a635d2ee8cdb1c2563c56a218b1cfac
jamieberridge <jamie.berridge@icloud.com>
2024-04-01 14:58:12 +0100

    updated for better manipulation

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-userInput = input("choose (1)weekend alcohol consumption and grade(2)age distribution of students (3)Relationship Status by Parental Cohabitation (4)travel vs study time")
+# List of visualisations formatted in a list to allow for easy manipulation
+listOfVisualisations = [
+    # Bailey's trends
+    "(1) Weekend alcohol consumption and grade", 
+    "(2) Age distribution of students",
+    "(3) Relationship Status by Parental Cohabitation",
+    "(4) Travel vs study time",
 
-if userInput == "1":
-    with open("grade-alcoholConsumption.py") as file:
-        exec(file.read())
-elif userInput == "2":
-    with open("histogram.py") as file:
-        exec(file.read())
-elif userInput == "3":
-    with open("romantic.py") as file:
-        exec(file.read())
-elif userInput == "4":
-    with open("studyTime.py") as file:
-        exec(file.read())
+    "(0) Exit"
+    ]
+
+# Loop to  display the visualisation options and allow the user to select another option or exit
+while True:
+    print("Choose a visualisation from the following: ")
+    for x in listOfVisualisations:  
+        print(x)
+
+    userInput = input("- ")
+    if userInput == "0":
+        break
+    elif userInput == "1":
+        with open("grade-alcoholConsumption.py") as file:
+            exec(file.read())
+    elif userInput == "2":
+        with open("histogram.py") as file:
+            exec(file.read())
+    elif userInput == "3":
+        with open("romantic.py") as file:
+            exec(file.read())
+    elif userInput == "4":
+        with open("studyTime.py") as file:
+            exec(file.read())
 
